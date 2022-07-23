802e356633d11a6097f96e42063cf4d3da33a74d
Andrew Leshishin <andreybogdanov347@gmail.com>
2022-07-23 14:09:53 +0300

    long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] analytics = {5, 4, 7, 9, 14};
+        long[] analytics = {5, 4, 7, 9, 14};
         SalesManager salesManager = new SalesManager(analytics);
         System.out.println(salesManager.max());
     }
